1c34665a9e0f084341d793edf9e9aa737ef9eb55
notepadewindos <vladossicov@gmail.com>
2024-11-05 20:17:56 +0200

    добрий день

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-class cat:
-    def __init__(self):
-        self.happines = 20
-        self.hp = 10
-cat = cat()
-while cat.happines < 60:
-    print("рівень щастя", cat.happines, "хп", cat.hp)
-    vubir = input("пограти з хазяїном (P)""поїсти (E)")
-    if vubir == "P":
-        cat.happines += 10
-    if vubir == "E":
-        cat.hp += 5
-print("ви щасливий кіт")
+print("добрий день")
