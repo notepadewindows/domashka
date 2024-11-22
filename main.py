e1ff6b3c47d3180fea283d10781e0886b3954e74
notepadewindos <vladossicov@gmail.com>
2024-11-22 12:43:07 +0200

    дз 7

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import time
-def tim ():
-    start = input("старт S")
-    if start == "S":
-        timestart = time.time()
-        end = input("кінець E")
-        if end == "E":
-            timeend = time.time()
-            timee = round(timeend - timestart)
-            print("функція працювала",timee,"секунд")
-tim()
+class gen:
+   def __init__(self, data):
+       self.data = data
+   def __iter__(self):
+       for item in self.data:
+           yield item
+iter = gen([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+for item in iter:
+   print(item)
